b405be3e871083e92eab16ec9566edabeef559d4
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2021-12-24 16:21:08 -0500

    Cleanup codebase
    Removed unused/commented code, and unnecessary string interpolation

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,10 +14,6 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-//    @State private var alertTitle = ""
-//    @State private var alertMessage = ""
-//    @State private var showingAlert = false
-    
     var recommendedBedTime: String {
         let config = MLModelConfiguration()
         guard let model = try? SleepCalculator(configuration: config) else {
@@ -46,69 +42,29 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section("When do you want to wake up?") {
-//                    Text("When do you want to wake up?")
-//                        .font(.headline)
-                    
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
                 
                 Section("Desired amount of sleep") {
-//                    Text("Desired amount of sleep")
-//                        .font(.headline)
-                    
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
                 
                 Section("Daily coffee intake") {
-//                    Text("Daily coffee intake")
-//                        .font(.headline)
                     Picker("Number of cups", selection: $coffeeAmount) {
                         ForEach(1..<21) {
                             Text($0 == 1 ? "1 cup" : "\($0) cups")
                         }
                     }
-                    
-                    //Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                 }
                 
                 Section("Recommended bedtime") {
-                    Text("\(recommendedBedTime)")
+                    Text(recommendedBedTime)
                 }
             }
             .navigationTitle("BetterRest")
-//            .toolbar {
-//                Button("Calculate", action: calculateBedtime)
-//            }
-//            .alert(alertTitle, isPresented: $showingAlert) {
-//                Button("OK") { }
-//            } message: {
-//                Text(alertMessage)
-//            }
         }
     }
-    
-//    func calculateBedtime() {
-//        do {
-//            let config = MLModelConfiguration()
-//            let model = try SleepCalculator(configuration: config)
-//
-//            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-//            let hour = (components.hour ?? 0) * 60 * 60
-//            let minute = (components.minute ?? 0) * 60
-//
-//            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-//
-//            let sleepTime = (wakeUp - prediction.actualSleep).formatted(date: .omitted, time: .shortened)
-////            alertTitle = "Your ideal bedtime is..."
-////            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-//        } catch {
-////            alertTitle = "Error"
-////            alertMessage = "Sorry, there was a problem calculating your bedtime."
-//        }
-//
-////        showingAlert = true
-//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
