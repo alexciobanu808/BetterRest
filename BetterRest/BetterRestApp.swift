af375ab0b74e09ba74ef107af3902ce77c2eaef2
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2021-12-24 15:54:34 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alex Ciobanu on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
